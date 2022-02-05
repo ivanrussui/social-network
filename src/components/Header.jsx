67a7ef14c33577367d15f21cb2e258cd6ab939d3
Ivan <fenomen_ny@mail.ru>
2022-02-05 20:38:11 +0300

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://logojinni.com/ru/image/logos/react-446.svg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          alt="img"
+        />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
