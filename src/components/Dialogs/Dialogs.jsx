5370e3073ec1968727bccc085df248bfab569c76
Ivan <fenomen_ny@mail.ru>
2022-02-10 22:59:54 +0300

    вурстка страницы Диалоги

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={`${s.dialog} + ${s.active}`}>
+                    Ivan
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Vladimir
+                </div>
+                <div className={s.dialog}>
+                    Kristina
+                </div>
+                <div className={s.dialog}>
+                    Vasilyi
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>My name is Ivan</div>
+                <div className={s.message}>And you?</div>
+            </div>
         </div>
     )
 }
