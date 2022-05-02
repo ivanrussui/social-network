1f865bcb7b31da52de9b99343af7c7d9dcd81946
Ivan <fenomen_ny@mail.ru>
2022-05-03 00:39:27 +0300

    redux

---
+++ src/redux/reduxStore.js
@@ -0,0 +1,16 @@
+import {combineReducers, createStore, legacy_createStore} from 'redux';
+import profileReducer from './profileReducer';
+import dialogsReducer from './dialogsReducer';
+import sidebarReducer from './sidebarReducer';
+
+
+let reducers = combineReducers({
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  sidebar: sidebarReducer
+});
+
+// let store = createStore(reducers);
+let store = legacy_createStore(reducers);
+
+export default store;
