2859a28951c7b55e11303fbb3ddc229b09c8b795
Ivan <fenomen_ny@mail.ru>
2022-05-25 23:21:06 +0300

    usersReducer 2

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -6,7 +6,7 @@ import sidebarReducer from './sidebarReducer';
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
-  sidebar: sidebarReducer
+  sidebar: sidebarReducer,
 });
 
 let store = legacy_createStore(reducers);
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,23 +1,29 @@
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+
 let initialState = {
+  users: [
+    {id: 1, followed: false, name: 'Ivan', status: 'Hello World', location: {country: 'Russia', city: 'Moscow'}},
+    {id: 2, followed: false, name: 'Dmitry', status: 'Yo Yo Yo', location: {country: 'Belarus', city: 'Minsk'}},
+    {id: 3, followed: true, name: 'Fedor', status: 'I am programmer', location: {country: 'Moldova', city: 'Kishinev'}},
+    {id: 4, followed: true, name: 'Vasily', status: 'Hi Developers', location: {country: 'USA', city: 'New York'}}
+  ]
   // ninjaTurtles: [
   //   {id: 1, name: 'Leonardo', role: 'Leader', weapon: 'Ninjato swords', color: 'Blue'},
   //   {id: 2, name: 'Raphael', role: 'Strong', weapon: 'Sai daggers', color: 'Red'},
   //   {id: 3, name: 'Donatello', role: 'Genius', weapon: 'Bo pole', color: 'Violet'},
   //   {id: 4, name: 'Michelangelo', role: 'Funny', weapon: 'Nunchaku', color: 'Orange'},
   // ]
-  users: [
-    {id: 1, name: 'Ivan', status: 'Hello World', location: {country: 'Russia', city: 'Moscow'} },
-    {id: 2, name: 'Dmitry', status: 'Yo Yo Yo', location: {country: 'Belarus', city: 'Minsk'} },
-    {id: 3, name: 'Fedor', status: 'I am programmer', location: {country: 'Moldova', city: 'Kishinev'} },
-    {id: 4, name: 'Vasily', status: 'Hi Developers', location: {country: 'USA', city: 'New York'} }
-  ]
 };
 
-// const usersReducer = (state = initialState, action) = {
-//   switch (action.type) {
-//     default:
-//       return state;
-//   }
-// }
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
-// export default usersReducer;
+export const followActionCreator = () => ({type: FOLLOW});
+export const unfollowActionCreator = () => ({type: UNFOLLOW});
+
+export default usersReducer;
