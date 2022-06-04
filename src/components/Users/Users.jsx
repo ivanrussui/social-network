4801d7acf31c6fea708ac4bd41cfd43b3d17e8be
Ivan <fenomen_ny@mail.ru>
2022-06-04 17:52:26 +0300

    Side Effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,22 +4,16 @@ import userAva from '../../assets/img/user.png';
 
 const Users = (props) => {
 
-  if (props.users.length === 0) {
-
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      // debugger;
-      props.setUsers(response.data.items)
-    });
-
-    // props.setUsers ( [
-    //   {id: 1, imgSrc: 'https://cdn-icons-png.flaticon.com/512/149/149452.png', followed: false, name: 'Ivan', status: 'Hello World', location: {country: 'Russia', city: 'Moscow'}},
-    //   {id: 2, imgSrc: 'https://cdn-icons-png.flaticon.com/512/149/149452.png', followed: false, name: 'Dmitry', status: 'Yo Yo Yo', location: {country: 'Belarus', city: 'Minsk'}},
-    //   {id: 3, imgSrc: 'https://cdn-icons-png.flaticon.com/512/149/149452.png', followed: true, name: 'Fedor', status: 'I am programmer', location: {country: 'Moldova', city: 'Kishinev'}},
-    //   {id: 4, imgSrc: 'https://cdn-icons-png.flaticon.com/512/149/149452.png', followed: true, name: 'Vasily', status: 'Hi Developers', location: {country: 'USA', city: 'New York'}}
-    // ])
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        props.setUsers(response.data.items)
+      });
+    }
   }
 
   return <div className={styles.users}>
+    <button onClick={getUsers}>Get Users</button>
     {
       props.users.map(u => <div key={u.id}>
         <div>
