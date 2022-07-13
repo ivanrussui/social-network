a70a28bf345768dcf65e1e069d7b796b76117bcf
Ivan <fenomen_ny@mail.ru>
2022-07-13 15:09:45 +0300

    profile page, ajax, api 5

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -17,10 +17,26 @@ const ProfileInfo = (props) => {
         />
       </div>
       <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large} alt="avatar" />
+        <img src={props.profile.photos.large} alt="avatar"/>
         <div className={s.descriptionText}>
           <div><b>О себе:</b> {props.profile.aboutMe}</div>
-          {/*<div>Поиск работы: {props.profile.lookingForAJob}</div>*/}
+          <div><b>Контакты:</b> <br/>
+              <a href="facebook.com">{props.profile.contacts.facebook}</a> <br/>
+              {/*<a href="#">{props.profile.contacts.website}</a> <br/>*/}
+              <a href="vk.com/dimych">{props.profile.contacts.vk}</a> <br/>
+              <a href="instagra.com/sds">{props.profile.contacts.instagram}</a> <br/>
+              {/*<a href="#">{props.profile.contacts.youtube}</a> <br/>*/}
+              <a href="github.com">{props.profile.contacts.github}</a> <br/>
+              {/*<a href="#">{props.profile.contacts.mainLink}</a> <br/>*/}
+          </div>
+
+          <div className={s.status}><b>Статус работы: </b>
+            {props.profile.lookingForAJob ?
+              <img src="https://avatanplus.com/files/resources/original/5693e5540266115231b7d02a.png" alt="smile"/> :
+              // 2 смайла на выбор)
+              // <img src="http://cdn.onlinewebfonts.com/svg/img_437392.png" alt="smile sad"/>}
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sert_-_sad_smile.svg/640px-Sert_-_sad_smile.svg.png" alt="smile sad"/>}
+          </div>
           <div><b>Поиск работы:</b> {props.profile.lookingForAJobDescription}</div>
           <div><b>Полное имя:</b> {props.profile.fullName}</div>
         </div>
