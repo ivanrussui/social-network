ef8825e20391c5d991bb2b35b4861051a83d6674
Ivan <fenomen_ny@mail.ru>
2022-07-28 22:00:48 +0300

    contacts API

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,6 +10,7 @@ const ProfileInfo = (props) => {
   }
 
   const photos = props.profile.photos.large;
+  const contacts = props.profile.contacts;
 
   return (
     <div>
@@ -26,19 +27,23 @@ const ProfileInfo = (props) => {
         }
         <div className={s.descriptionText}>
           <div><b>О себе:</b> {props.profile.aboutMe}</div>
-          <div><b>Контакты:</b> <br/>
+          { contacts.facebook || contacts.vk || contacts.instagram || contacts.github ?
+            <div><b>Контакты:</b> <br/>
               <a href="facebook.com">{props.profile.contacts.facebook}</a> <br/>
               <a href="vk.com/dimych">{props.profile.contacts.vk}</a> <br/>
               <a href="instagra.com/sds">{props.profile.contacts.instagram}</a> <br/>
               <a href="github.com">{props.profile.contacts.github}</a> <br/>
-          </div>
+            </div> :
+            <p><b style={{ color: 'red' }}>Контакты не указаны</b></p>
+          }
 
           <div className={s.status}><b>Статус работы: </b>
-            {props.profile.lookingForAJob ?
+            { props.profile.lookingForAJob ?
               <img src="https://avatanplus.com/files/resources/original/5693e5540266115231b7d02a.png" alt="smile"/> :
               // 2 смайла на выбор)
               // <img src="http://cdn.onlinewebfonts.com/svg/img_437392.png" alt="smile sad"/>}
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sert_-_sad_smile.svg/640px-Sert_-_sad_smile.svg.png" alt="smile sad"/>}
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sert_-_sad_smile.svg/640px-Sert_-_sad_smile.svg.png" alt="smile sad"/>
+            }
           </div>
           <div><b>Поиск работы:</b> {props.profile.lookingForAJobDescription}</div>
           <div><b>Полное имя:</b> {props.profile.fullName}</div>
