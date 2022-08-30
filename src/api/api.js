a9b5ecef9548fb6f482fe2db04f9adaeb0967c09
Ivan <fenomen_ny@mail.ru>
2022-08-30 18:41:46 +0300

    WIP

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,16 @@
 import * as axios from "axios";
 
-const baseUrl = 'https://social-network.samuraijs.com/api/';
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
 
 export const getUsers = (currentPage = 1, pageSize = 5) => {
-  return axios.get(baseUrl + `1.0/users?page=${currentPage}&count=${pageSize}`, {
+  return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
     withCredentials: true // для кроссдоменных запросов передаем что мы авторизованы
   })
     .then(response => response.data);
 };
 
 export const deleteUser = (id) => {
-  return axios.delete(baseUrl + `1.0/follow/${id}`, {
+  return axios.delete(baseUrl + `follow/${id}`, {
     withCredentials: true,
     headers: {
       'API-KEY': 'f46c8a87-f5e7-4a08-b342-9cddf97694b1'
@@ -20,7 +20,7 @@ export const deleteUser = (id) => {
 };
 
 export const postUser = (id) => {
-  return axios.post(baseUrl + `1.0/follow/${id}`, {}, {
+  return axios.post(baseUrl + `follow/${id}`, {}, {
     withCredentials: true,
     headers: {
       'API-KEY': 'f46c8a87-f5e7-4a08-b342-9cddf97694b1'
@@ -30,11 +30,11 @@ export const postUser = (id) => {
 };
 
 export const getProfile = (profileId = 2) => {
-  return axios.get(baseUrl + `1.0/profile/${profileId}`).then(response => response.data);
+  return axios.get(baseUrl + `profile/${profileId}`).then(response => response.data);
 }
 
 export const getAuthMe = () => {
-  return axios.get(baseUrl + `1.0/auth/me`, {
+  return axios.get(baseUrl + `auth/me`, {
     withCredentials: true
   })
     .then(response => response.data);
