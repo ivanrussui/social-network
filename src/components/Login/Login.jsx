5673de21a92288ec6ced33880019057e70f5afdc
Ivan <fenomen_ny@mail.ru>
2023-01-16 19:38:16 +0300

    wip перед установкой redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,38 @@
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'}/>
+            </div>
+            <div>
+                <input placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/>remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 const Login = () => {
     return (
-        <a style={{
-            display: 'inline-block',
-            fontSize: '2em',
-            margin: '0.67em',
-            fontWeight: 'bold',
-            textDecoration: 'none'
-        }}
-           target={"_blank"} href={'https://social-network.samuraijs.com/'}>LOGIN</a>
+        <>
+            <a style={{
+                display: 'inline-block',
+                fontSize: '2em',
+                margin: '0.67em',
+                fontWeight: 'bold',
+                textDecoration: 'none'
+            }}
+               target={'_blank'} href={'https://social-network.samuraijs.com/'}>LOGIN</a>
+            <LoginForm/>
+        </>
     )
     // return <h1 style={{paddingLeft: '10px', color: 'indigo'}}>LOGIN</h1>
 }
+
 
 export default Login;
