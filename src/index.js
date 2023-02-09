0ca898739d7068c11b9950bcb48a31ec310c4112
Ivan <fenomen_ny@mail.ru>
2023-02-09 22:37:35 +0300

    feat reselect lib

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {BrowserRouter} from 'react-router-dom';
 import store from './redux/reduxStore';
 import {Provider} from "react-redux";
 
-// setInterval(() => {
-//     store.dispatch({type: 'FAKE'})
-// }, 1000);
+setInterval(() => {
+    store.dispatch({type: 'FAKE'})
+}, 1000);
 
 ReactDOM.render(
     <BrowserRouter>
